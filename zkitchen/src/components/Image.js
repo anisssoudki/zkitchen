b8cc8ac21881ed309497b339ac41a87abc25b0e9
Aniss <59289660+anisssoudki@users.noreply.github.com>
2021-03-21 12:14:34 -0700

    Update Image.js

--- zkitchen/src/components/Image.js
+++ zkitchen/src/components/Image.js
@@ -5,7 +5,7 @@ const Image = () => {
     return (
         <div>
           <img className="img"  width="auto"
-    height= "auto" src="https://raw.githubusercontent.com/anisssoudki/img/main/me.jpg?token=AOELAPCUEIY6KKIVYP75WUTAK6MYS"/>
+   src="https://raw.githubusercontent.com/anisssoudki/img/main/me.jpg?token=AOELAPCUEIY6KKIVYP75WUTAK6MYS"/>
 
    
         </div>
