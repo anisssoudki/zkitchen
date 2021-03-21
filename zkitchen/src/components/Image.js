f0c3abee9e1a60936fca48664038a6c1cea77d2f
Aniss <59289660+anisssoudki@users.noreply.github.com>
2021-03-21 12:10:00 -0700

    Update Image.js

--- zkitchen/src/components/Image.js
+++ zkitchen/src/components/Image.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Image = () => {
     return (
         <div>
-          <img className="img"  width="250"
-    height= "150" src="https://raw.githubusercontent.com/anisssoudki/img/main/me.jpg?token=AOELAPCUEIY6KKIVYP75WUTAK6MYS"/>
+          <img className="img"  width="286"
+    height= "306" src="https://raw.githubusercontent.com/anisssoudki/img/main/me.jpg?token=AOELAPCUEIY6KKIVYP75WUTAK6MYS"/>
 
    
         </div>
